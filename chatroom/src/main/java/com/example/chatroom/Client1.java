165aa05fe5e451cbfe0cc9b6aecb15c480346e70
Zahra <zahraebrahimi8338git@gmail.com>
2023-06-19 01:06:02 +0330

    2nd_true_commit GetInformation.java(receiving information in this class and set them as Clients attribute)

--- chatroom/src/main/java/com/example/chatroom/Client1.java
+++ chatroom/src/main/java/com/example/chatroom/Client1.java
@@ -6,10 +6,21 @@ public class Client1 {
     static String userName , passWord , bio;
     static int birthYear;
     public static void main(String[] args) {
+        GetInformation getInformation = new GetInformation();
+        getInformation.getInformation();
+    }
+}
+class GetInformation {
+    void getInformation() {
         Scanner sc = new Scanner(System.in);
-        userName=sc.nextLine();
-        passWord=sc.nextLine();
-        birthYear=sc.nextInt();sc.nextLine();
-        bio=sc.nextLine(); sc.nextLine();
+        System.out.println("userName : ");
+        Client1.userName = sc.nextLine();
+        System.out.println("passWord : ");
+        Client1.passWord = sc.nextLine();
+        System.out.println("birthYear : ");
+        Client1.birthYear = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Bio : ");
+        Client1.bio = sc.nextLine(); // receive information
     }
 }
