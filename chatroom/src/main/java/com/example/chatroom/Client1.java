ba3fe4b8a43baa646c638705dcee6ec0cbb2cf04
Zahra <zahraebrahimi8338git@gmail.com>
2023-06-19 01:10:29 +0330

    3rd_true_commit GetInformation.java(add saveInformationByDataBase function and save information we receive to dataBase)

--- chatroom/src/main/java/com/example/chatroom/Client1.java
+++ chatroom/src/main/java/com/example/chatroom/Client1.java
@@ -1,5 +1,8 @@
 package com.example.chatroom;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Client1 {
@@ -22,5 +25,24 @@ class GetInformation {
         sc.nextLine();
         System.out.println("Bio : ");
         Client1.bio = sc.nextLine(); // receive information
+        saveInformationByDataBase();
+    }
+    void saveInformationByDataBase(){
+        String URL = "jdbc:mysql://localhost/clientinformation"; // address of the database we use
+        String UserName ="root";
+        String PassWord ="123";
+        try{ // need try catch to see if ant=y Exception will happen
+            //Class.forName("com.mysql.jdbc.Driver"); // code says we dont need this
+            Connection connection = DriverManager.getConnection(URL ,UserName , PassWord); // we need a connection to dataBase
+            System.out.println("connected");
+            String sqlCmd =String.format("INSERT INTO `information` (`UserName`, `PassWord`, `BirthYear` , `Bio` ) VALUES ('%s', '%s', '%d' , '%s' )" ,Client1.userName , Client1.passWord ,Client1.birthYear ,Client1.bio);
+            Statement statement = connection.prepareStatement(sqlCmd); // need statement to prepare command
+            statement.execute(sqlCmd); // do the command
+            connection.close();
+            System.out.println("finished");
+        }
+        catch (Exception ex){
+            return;
+        }
     }
 }
