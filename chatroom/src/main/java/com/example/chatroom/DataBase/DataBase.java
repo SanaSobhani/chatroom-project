553135fd806c33d2fe8527538305ecb7fe49ba86
Zahra <zahraebrahimi8338git@gmail.com>
2023-06-26 12:54:31 +0330

    commit on branch zahra DatBase.java and constructor method

--- chatroom/src/main/java/com/example/chatroom/DataBase/DataBase.java
+++ chatroom/src/main/java/com/example/chatroom/DataBase/DataBase.java
@@ -1,4 +1,17 @@
 package com.example.chatroom.DataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBase {
+    String URL = "jdbc:mysql://localhost/messager"; // address of the database we use
+    String UserName ="root";
+    String PassWord ="123";
+    Connection connection;
+    public DataBase() throws SQLException, ClassNotFoundException {
+        connection = DriverManager.getConnection(URL , UserName , PassWord);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+    }
 }
